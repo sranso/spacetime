2c856aefaa585e2b73e14116922d3e331cd3606c
Jake Sandlund <jake@jakesandlund.com>
2015-01-09 21:48:03 -0600

    s/statement/step/

--- public/js/untext.js
+++ public/js/untext.js
@@ -1,9 +1,9 @@
-var camera, allStatements, mouse, textInput, under, groups, selection, selectionHistory, __selectionHistoryAll, selectionStart, selectionEnd, selectionHistoryEl;
+var camera, allSteps, mouse, textInput, under, groups, selection, selectionHistory, __selectionHistoryAll, selectionStart, selectionEnd, selectionHistoryEl;
 
-var statementsX = 160;
-var statementsTextX = 50;
+var stepsX = 160;
+var stepsTextX = 50;
 var lineHeight = 35;
-var statementW = 400;
+var stepW = 400;
 
 mouse = null;
 under = null;
@@ -42,22 +42,22 @@ var drawSetup = function () {
         .attr('height', 20000) ;
 
     textInput = d3.select('#text-input')
-        .style('left', (statementsX + statementsTextX + 23) + 'px') ;
+        .style('left', (stepsX + stepsTextX + 23) + 'px') ;
 
     textInput.select('input')
-        .style('width', (statementW - statementsTextX - 20) + 'px')
+        .style('width', (stepW - stepsTextX - 20) + 'px')
         .style('height', (lineHeight - 12) + 'px') ;
 
     camera.append('rect')
         .classed('track-rail', true)
-        .attr('x', statementsX + 70)
+        .attr('x', stepsX + 70)
         .attr('y', 10)
         .attr('width', 10)
         .attr('height', 10000) ;
 
     camera.append('rect')
         .classed('track-rail', true)
-        .attr('x', statementsX + statementW - 80)
+        .attr('x', stepsX + stepW - 80)
         .attr('y', 10)
         .attr('width', 10)
         .attr('height', 10000) ;
@@ -128,14 +128,14 @@ var changeSelection = function () {
     if (under) {
         selectionEnd = under;
     }
-    var startI = _.indexOf(allStatements, selectionStart);
-    var endI = _.indexOf(allStatements, selectionEnd);
+    var startI = _.indexOf(allSteps, selectionStart);
+    var endI = _.indexOf(allSteps, selectionEnd);
     if (endI < startI) {
         var temp = startI;
         startI = endI;
         endI = temp;
     }
-    selection.elements = allStatements.slice(startI, endI + 1);
+    selection.elements = allSteps.slice(startI, endI + 1);
 
     computePositions();
     draw();
@@ -147,21 +147,21 @@ var stopSelection = function () {
 };
 
 var computePositions = function () {
-    computeTrackPositions(allStatements);
+    computeTrackPositions(allSteps);
     computeSelectionHistoryPositions();
 };
 
-var computeTrackPositions = function (statements) {
+var computeTrackPositions = function (steps) {
     var prevPos = {x: 0, y: 0, w: 0, h: 0};
-    _.each(statements, function (statement) {
+    _.each(steps, function (step) {
         var pos = {
-            x: statementsX,
+            x: stepsX,
             y: prevPos.y + lineHeight,
-            w: statementW,
+            w: stepW,
             h: lineHeight - 3,
         };
-        statement.position = pos;
-        _.extend(statement, pos);
+        step.position = pos;
+        _.extend(step, pos);
         prevPos = pos;
     });
 };
@@ -184,15 +184,15 @@ var computeSelectionHistoryPositions = function () {
 };
 
 var draw = function () {
-    drawTrack(allStatements);
+    drawTrack(allSteps);
     drawSelectionHistory();
 };
 
-var drawTrack = function (statements) {
-    var statementEls = camera.selectAll('g.statement')
-        .data(statements) ;
+var drawTrack = function (steps) {
+    var stepEls = camera.selectAll('g.step')
+        .data(steps) ;
 
-    var statementEnterEls = statementEls.enter().append('g')
+    var stepEnterEls = stepEls.enter().append('g')
         .attr('transform', function (d, i) {
             return 'translate(' + d.x + ',' + d.y + ')';
         })
@@ -200,7 +200,7 @@ var drawTrack = function (statements) {
             d.__el__ = this;
         }) ;
 
-    statementEnterEls.append('rect')
+    stepEnterEls.append('rect')
         .classed('background', true)
         .attr('x', 0)
         .attr('y', 0)
@@ -209,21 +209,21 @@ var drawTrack = function (statements) {
         .attr('width', _.property('w'))
         .attr('height', _.property('h')) ;
 
-    statementEnterEls.append('text')
+    stepEnterEls.append('text')
         .attr('y', 21)
-        .attr('x', statementsTextX) ;
+        .attr('x', stepsTextX) ;
 
-    statementEls
+    stepEls
         .attr('class', function (d) {
             var classes = [];
             if (_.contains(selection.elements, d)) {
                 classes.push('selection');
             }
-            classes.push('statement');
+            classes.push('step');
             return classes.join(' ');
         }) ;
 
-    statementEls.select('text')
+    stepEls.select('text')
         .text(_.property('text')) ;
 };
 
@@ -290,23 +290,23 @@ var findUnderMouse = function () {
 };
 
 var findFromCoordinates = function (x, y) {
-    return _.find(allStatements, function (statement) {
-        if (statement.y < y && y < statement.y + statement.h) {
-            return statement.x < x && x < statement.x + statement.w;
+    return _.find(allSteps, function (step) {
+        if (step.y < y && y < step.y + step.h) {
+            return step.x < x && x < step.x + step.w;
         }
         return false;
     });
 };
 
-var createStatement = function (statement) {
+var createStep = function (step) {
     return _.extend({
         text: '',
         position: null,
         __el__: null,
-    }, statement);
+    }, step);
 };
 
-allStatements = _.map([
+allSteps = _.map([
     {text: '4 + 1'},
     {text: '^ * 3'},
     {text: '^ - 12'},
@@ -328,7 +328,7 @@ allStatements = _.map([
     {text: '3 + 5'},
     {text: 'square width:50 x:100 y:150'},
     {text: 'square width:100 x:100 y:150'},
-], createStatement);
+], createStep);
 
 var keyMap = {8: 'backspace', 9: 'tab', 13: 'enter', 16: 'shift', 17: 'ctrl', 18: 'alt', 19: 'pause/break', 20: 'caps lock', 27: 'escape', 32: 'space', 33: 'page up', 34: 'page down', 35: 'end', 36: 'home', 37: 'left arrow', 38: 'up arrow', 39: 'right arrow', 40: 'down arrow', 45: 'insert', 46: 'delete', 48: '0', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9', 65: 'A', 66: 'B', 67: 'C', 68: 'D', 69: 'E', 70: 'F', 71: 'G', 72: 'H', 73: 'I', 74: 'J', 75: 'K', 76: 'L', 77: 'M', 78: 'N', 79: 'O', 80: 'P', 81: 'Q', 82: 'R', 83: 'S', 84: 'T', 85: 'U', 86: 'V', 87: 'W', 88: 'X', 89: 'Y', 90: 'Z', 91: 'left window key', 92: 'right window key', 93: 'select key', 96: 'numpad 0', 97: 'numpad 1', 98: 'numpad 2', 99: 'numpad 3', 100: 'numpad 4', 101: 'numpad 5', 102: 'numpad 6', 103: 'numpad 7', 104: 'numpad 8', 105: 'numpad 9', 106: 'multiply', 107: 'add', 109: 'subtract', 110: 'decimal point', 111: 'divide', 112: 'F1', 113: 'F2', 114: 'F3', 115: 'F4', 116: 'F5', 117: 'F6', 118: 'F7', 119: 'F8', 120: 'F9', 121: 'F10', 122: 'F11', 123: 'F12', 144: 'num lock', 145: 'scroll lock', 186: ';', 187: '=', 188: ',', 189: '-', 190: '.', 191: '/', 192: '`', 219: '[', 220: '\\', 221: ']', 222: "'"};
 
