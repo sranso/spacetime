614bdca19cf2f5e447866a2704c4622411c38b97
Jake Sandlund <jake@jakesandlund.com>
2015-11-21 17:02:13 -0600

    Update deploy.sh for new Node.js build

--- deploy.sh
+++ deploy.sh
@@ -9,27 +9,16 @@ if [ -z "$tag" ]; then
     exit 1
 fi
 
-if [ ! -d workspace ]; then
-    mkdir -p workspace/dist
-    mkdir -p releases/current
-    git clone git@github.com:jakesandlund/spacetime.git workspace/spacetime
-fi
+./init.sh
 
-cd workspace/spacetime
+cd workspace
+
+cd spacetime
 git fetch --tags
 git checkout "$tag"
-./build.sh
-cd ../..
-
-rm -r workspace/dist
-mkdir workspace/dist
-
-node_modules/.bin/cleancss -o workspace/dist/styles.css workspace/spacetime/dist/styles.css
-
-node_modules/.bin/uglifyjs workspace/spacetime/dist/scripts.js --compress -o workspace/dist/scripts.js
-
-cp workspace/spacetime/dist/index.html workspace/dist/index.html
-cp -R workspace/spacetime/dist/vendor workspace/dist/vendor
+cd ..
+../build.js
+cd ..
 
 branch=$(echo "$tag" | cut -d '.' -f 1)
 timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ" | sed 's/:/_/g')
