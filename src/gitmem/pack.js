6e0f5de208acca5aa6f0bda2d9446f90a748b0d9
Jake Sandlund <jake@jakesandlund.com>
2016-01-31 17:28:19 -0600

    Create packs. (equivalent to real commit at z-lib level 6)

---
+++ src/gitmem/pack.js
@@ -0,0 +1,118 @@
+'use strict';
+global.Pack = {};
+(function () {
+
+Pack.create = function (files) {
+    var pack = new Uint8Array(512);
+    pack[0] = 0x50; // P
+    pack[1] = 0x41; // A
+    pack[2] = 0x43; // C
+    pack[3] = 0x4b; // K
+
+    pack[7] = 2;
+
+    pack[8] = files.length >>> 24;
+    pack[9] = (files.length >>> 16) & 0xff;
+    pack[10] = (files.length >>> 8) & 0xff;
+    pack[11] = files.length & 0xff;
+    var j = 12;
+
+    var f;
+    for (f = 0; f < files.length; f++) {
+        // Assume deflated length + header is less than 100
+        // bytes over file length (inflated with type header).
+        while (pack.length - j < files[f].length + 100) {
+            if (pack.length < 32768) {
+                var newLength = pack.length * 2;
+            } else {
+                var newLength = 8192 * (((pack.length * 1.5) >>> 13) + 1);
+            }
+            var newPack = new Uint8Array(newLength);
+            var i;
+            for (i = 0; i < pack.length; i++) {
+                newPack[i] = pack[i];
+            }
+            pack = newPack;
+        }
+        j += Pack._packFile(pack, j, files[f]);
+    }
+
+    var remaining = pack.length - j;
+    if (remaining < 20) {
+        var newPack = new Uint8Array(pack.length + 20 - remaining);
+        var i;
+        for (i = 0; i < pack.length; i++) {
+            newPack[i] = pack[i];
+        }
+        pack = newPack;
+    }
+
+    Sha1.hash(pack.subarray(0, j), pack, j);
+
+    return pack.subarray(0, j + 20);
+}
+
+Pack._packFile = function (pack, packOffset, file) {
+    var i = file.indexOf(0x20, 4);
+    var type = String.fromCharCode.apply(null, file.subarray(0, i));
+    i = file.indexOf(0, j) + 1;
+    var length = file.length - i;
+
+    if (length < 32768) {
+        // Try to only need one chunk.
+        var chunkSize = 4096 * ((length >>> 13) + 1);
+    } else {
+        // Shoot for 1/4 of the inflated size to reduce overhead.
+        var chunkSize = 8192 * (((length / 4) >>> 13) + 1);
+    }
+
+    var typeBits;
+    if (type === 'blob') {
+        typeBits = 0x30;
+    } else if (type === 'tree') {
+        typeBits = 0x20;
+    } else if (type === 'commit') {
+        typeBits = 0x10;
+    } else {
+        throw new Error('Unknown type: ' + type);
+    }
+
+    var c = typeBits | (length & 0xf);
+    var j = packOffset;
+
+    length >>= 4;
+    while (length) {
+        pack[j] = c | 0x80;
+        c = length & 0x7f;
+        length >>= 7;
+        j++;
+    }
+    pack[j] = c;
+
+    j++;
+    var deflate = new pako.Deflate({level: 6, chunkSize: chunkSize});
+    deflate.onData = deflateOnData;
+    deflate.j = j;
+    deflate.pack = pack;
+    deflate.onEnd = deflateOnEnd;
+
+    deflate.push(file.subarray(i), true);
+
+    return deflate.j - packOffset;
+};
+
+var deflateOnData = function (chunk) {
+    var j = this.j;
+    var pack = this.pack;
+    var i;
+    for (i = 0; i < chunk.length; i++) {
+        pack[j + i] = chunk[i];
+    }
+    this.j += i;
+};
+
+var deflateOnEnd = function (status) {
+    if (status !== 0) throw new error(this.strm.msg);
+}
+
+})();
---
+++ test/gitmem/test-pack.js
@@ -0,0 +1,63 @@
+var helper = require('../helper');
+var hex = helper.hex;
+
+var blob = Blob.fromString('foo bar\n');
+
+var deflated = pako.deflate(blob, {level: 1, chunkSize: 4096});
+log(hex(deflated));
+//=> 78014bcac94f52b06048cbcf57484a2ce20200268c049b
+
+var pack = new Uint8Array(15);
+var n = Pack._packFile(pack, 0, blob);
+log(n, pack[0].toString(16));
+//=> 17 '38'
+
+log(hex(pack.subarray(0, n)));
+//=> 38789c4bcbcf57484a2ce202000d14
+
+pack = Pack.create([blob]);
+log(hex(pack));
+//=> 5041434b000000020000000138789c4bcbcf57484a2ce202000d1402a4f6fdeefeaef7945dd30750fbcb3c45a6a163f183
+
+// Reconstructing actual git commit push
+var offsets = {};
+var tree = Tree.createSkeleton(offsets, {
+    'foobar.txt': 'blob',
+});
+Sha1.hash(blob, tree, offsets['foobar.txt']);
+
+var treeHash = new Uint8Array(20);
+Sha1.hash(tree, treeHash, 0);
+log(hex(treeHash));
+//=> 792e608d5b9141ebced75e4c7d91f2fc4f9da8b3
+
+var oldGetTimezoneOffset = Date.prototype.getTimezoneOffset;
+Date.prototype.getTimezoneOffset = function () {
+    return 360;
+};
+
+var author = {
+    name: 'Jake Sandlund',
+    email: 'jake@jakesandlund.com',
+    date: new Date(1454213963000),
+};
+
+var commitObject = {
+    tree: treeHash,
+    parents: [],
+    committer: author,
+    author: author,
+    message: 'Initial commit\n',
+};
+
+var commit = Commit.createFromObject(commitObject);
+var commitHash = new Uint8Array(20);
+Sha1.hash(commit, commitHash, 0);
+log(hex(commitHash));
+//=> 4e72110cbb91dd87f7b7eea22f5f0bcb233e95bf
+
+pack = Pack.create([commit, tree, blob]);
+log(hex(pack));
+//=> 5041434b00000002000000039d0b789c9d8c4b0ac2301400f739c5bb80927f1a90e256b79ee025ef05a36d0a35bdbf153d819b8199c5f4951942d4ece5402e456515a7cc141cdb1c28aaa24bb625120ec908dcfa7d59e18a4f861b369ab646707aec7afee0f54bc7bccc2328ebac56267a0307e9a5147b9d6beffcf7405c5aed1527f89ec41b30b73b78a602789c3334303033315148cbcf4f4a2cd22ba92861b856facbe5291bdba9e5ac87bff43ed0f9a879fce722000f8b111338789c4bcbcf57484a2ce202000d1402a48eaa2ed0693571e022227e5d5ae5f5d4df1ac88d
+
+Date.prototype.getTimezoneOffset = oldGetTimezoneOffset;
