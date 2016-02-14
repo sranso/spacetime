e2a1fb7bfcf21ae0ec9fcd042ac5a4eda443c3be
Jake Sandlund <jake@jakesandlund.com>
2016-02-13 23:03:30 -0600

    Use quiet and no side band for send pack.

--- src/gitmem/send-pack.js
+++ src/gitmem/send-pack.js
@@ -5,7 +5,7 @@ global.SendPack = {};
 SendPack.postPath = '/git-receive-pack';
 SendPack.postContentType = 'application/x-git-receive-pack-request';
 
-var capabilities = GitFile.stringToArray('report-status side-band-64k agent=gitmem/0.0.0');
+var capabilities = GitFile.stringToArray('report-status quiet agent=gitmem/0.0.0');
 var firstLineConstantLength = 4 + 40 + 1 + 40 + 1 + 2 + capabilities.length;
 var hexCharacters = GitFile.stringToArray('0123456789abcdef');
 
