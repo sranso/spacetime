499135ab7dc377d422e07712a1166a539476ee35
Jake Sandlund <jake@jakesandlund.com>
2016-02-27 22:14:52 -0600

    Blob working with $Heap

--- src/gitmem/blob.js
+++ src/gitmem/blob.js
@@ -12,68 +12,42 @@ Blob.initialize = function () {
     Blob.emptyEnd = emptyBlobRange[1];
     Blob.emptyHashOffset = $Heap.nextOffset;
     $Heap.nextOffset += 20;
-    Sha1.hash($, Blob.emptyBlobBegin, Blob.emptyBlobEnd, Blob.emptyBlobHashOffset);
+    Sha1.hash($, Blob.emptyBegin, Blob.emptyEnd, Blob.emptyHashOffset);
 };
 
 var blobPrefix = GitConvert.stringToArray('blob ');
 
 Blob.createFromString = function (string) {
     var lengthString = '' + string.length;
-    var blob = new Uint8Array(blobPrefix.length + lengthString.length + 1 + string.length);
+    var blobLength = blobPrefix.length + lengthString.length + 1 + string.length;
+    if ($Heap.nextOffset + blobLength > $Heap.capacity) {
+        FileSystem.resizeHeap($FileSystem, blobLength);
+    }
+    var blobBegin = $Heap.nextOffset;
+    $Heap.nextOffset += blobLength;
 
+    var blobj = blobBegin;
     var i;
     for (i = 0; i < blobPrefix.length; i++) {
-        blob[i] = blobPrefix[i];
+        $[blobj + i] = blobPrefix[i];
     }
 
-    var j = i;
+    blobj += i;
     for (i = 0; i < lengthString.length; i++) {
-        blob[j + i] = lengthString.charCodeAt(i);
+        $[blobj + i] = lengthString.charCodeAt(i);
     }
-    blob[j + i] = 0;
+    $[blobj + i] = 0;
 
-    j += i + 1;
+    blobj += i + 1;
     for (i = 0; i < string.length; i++) {
-        blob[j + i] = string.charCodeAt(i);
-    }
-    return blob;
-};
-
-Blob.createFromArray = function (array) {
-    var lengthString = '' + array.length;
-    var blob = new Uint8Array(blobPrefix.length + lengthString.length + 1 + array.length);
-
-    var i;
-    for (i = 0; i < blobPrefix.length; i++) {
-        blob[i] = blobPrefix[i];
+        $[blobj + i] = string.charCodeAt(i);
     }
 
-    var j = i;
-    for (i = 0; i < lengthString.length; i++) {
-        blob[j + i] = lengthString.charCodeAt(i);
-    }
-
-    j += i + 1;
-    for (i = 0; i < array.length; i++) {
-        blob[j + i] = array[i];
-    }
-    return blob;
+    return [blobBegin, blobBegin + blobLength];
 };
 
-Blob.parseArray = function (blob) {
-    return blob.subarray(blob.indexOf(0, 6) + 1);
+Blob.contentBegin = function (blobBegin) {
+    return $.indexOf(0, blobBegin + 6) + 1;
 };
-
-Blob.parseString = function (blob) {
-    return String.fromCharCode.apply(null, blob.subarray(blob.indexOf(0, 6) + 1));
-};
-
-Blob.parseStringOffset = function (blob, offset) {
-    return String.fromCharCode.apply(null, blob.subarray(blob.indexOf(0, 6) + 1 + offset));
-};
-
-Blob.emptyBlob = Blob.createFromString('');
-Blob.emptyBlobHash = new Uint8Array(20);
-Sha1.hash(Blob.emptyBlob, Blob.emptyBlobHash, 0);
 
 })();
--- test/gitmem/test-blob.js
+++ test/gitmem/test-blob.js
@@ -1,40 +1,28 @@
 'use strict';
 require('../helper');
 
-var blob = Blob.createFromString('foo');
-log(pretty(blob));
+global.$Heap = Heap.create(512);
+global.$ = $Heap.array;
+
+var blobRange = Blob.createFromString('foo');
+var blobBegin = blobRange[0];
+var blobEnd = blobRange[1];
+log(blobRange, blobBegin, blobEnd);
+//=> [ 0, 10 ] 0 10
+log(pretty(blobBegin, blobEnd));
 //=> blob 3\x00foo
-log(Blob.parseString(blob));
+var contentBegin = Blob.contentBegin(blobBegin);
+//=> 7
+log(GitConvert.arraySliceToString($, contentBegin, blobEnd));
 //=> foo
-log(Blob.parseStringOffset(blob, 1));
-//=> oo
-
-blob = Blob.createFromArray(new Uint8Array([0xff, 1, 2, 3]));
-log(pretty(blob));
-//=> blob 4\x00\xff\x01\x02\x03
-log(hex(Blob.parseArray(blob)));
-//=> ff010203
 
 
-var emptyHash = new Uint8Array(20);
-var emptyBlob = Blob.createFromString('');
-log('empty blob is "' + Blob.parseString(emptyBlob) + '"');
-//=> empty blob is ""
-
-Sha1.hash(emptyBlob, emptyHash, 0);
-log(hex(Blob.emptyBlobHash));
+Blob.initialize();
+log(Blob.emptyBegin, Blob.emptyEnd);
+//=> 10 17
+log(pretty(Blob.emptyBegin, Blob.emptyEnd));
+//=> blob 0\x00
+log(Blob.emptyHashOffset);
+//=> 17
+log(hash(Blob.emptyHashOffset));
 //=> e69de29bb2d1d6434b8b29ae775ad8c2e48c5391
-
-log(GitConvert.hashEqual(emptyHash, 0, Blob.emptyBlobHash, 0));
-//=> true
-
-var helloHash = new Uint8Array(20);
-var helloWorldBlob = Blob.createFromString('Hello, World!');
-log(Blob.parseString(helloWorldBlob));
-//=> Hello, World!
-Sha1.hash(helloWorldBlob, helloHash, 0);
-log(hex(helloHash));
-//=> b45ef6fec89518d314f546fd6c3025367b721684
-
-log(GitConvert.hashEqual(emptyHash, 0, helloHash, 0));
-//=> false
