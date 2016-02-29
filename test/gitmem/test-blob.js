dca4f389080d140449f3aea93902587f71749b68
Jake Sandlund <jake@jakesandlund.com>
2016-02-28 22:05:55 -0600

    Add forgotten test diff for Blob.initialize re-ordering.

--- test/gitmem/test-blob.js
+++ test/gitmem/test-blob.js
@@ -19,10 +19,10 @@ log(GitConvert.arraySliceToString($, contentStart, blobEnd));
 
 Blob.initialize();
 log(Blob.emptyStart, Blob.emptyEnd);
-//=> 10 17
+//=> 84 91
 log(pretty(Blob.emptyStart, Blob.emptyEnd));
 //=> blob 0\x00
 log(Blob.emptyHashOffset);
-//=> 17
+//=> 64
 log(hash(Blob.emptyHashOffset));
 //=> e69de29bb2d1d6434b8b29ae775ad8c2e48c5391
