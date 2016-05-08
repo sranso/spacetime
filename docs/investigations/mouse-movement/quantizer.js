5accd42410b9904f37fb04e41f6d311ea6fd150d
Jake Sandlund <jake@jakesandlund.com>
2016-05-07 19:41:48 -0500

    Separate out quantization stats and generation.

--- docs/investigations/mouse-movement/quantizer.js
+++ docs/investigations/mouse-movement/quantizer.js
@@ -2,51 +2,66 @@
 var Quantizer = {};
 (function () {
 
-var quantizeAtEachLevel = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14,15, 17, 19, 22, 25, 29, 33, 38, 44, 51, 59, 68, 78, 90];
-var countsAtEachLevel =   [18, 5, 3, 2, 2, 2, 1, 1, 1,  1,  1,  1,  1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  9];
+// These arrays are hand-crafted using trial and error.
+var gapAtEachLevel    = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14,15, 17, 19, 22, 25, 29, 33, 38, 44, 51, 59, 68, 78, 90];
+var countsAtEachLevel = [18, 5, 3, 2, 2, 2, 1, 1, 1,  1,  1,  1,  1, 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  9];
 
 Quantizer.generateQuantizations = function () {
     var quantizations = [];
 
-    // For stats only
+    var k;
+    for (k = 0; k < countsAtEachLevel.length; k++) {
+        var count = countsAtEachLevel[k];
+        var gap = gapAtEachLevel[k];
+
+        var j;
+        for (j = 0; j < count; j++) {
+            quantizations.push(1);
+
+            var i;
+            for (i = 1; i < gap; i++) {
+                quantizations.push(0);
+            }
+        }
+    }
+
+    quantizationStats();
+
+    return quantizations;
+};
+
+// These stats are used for feedback to adjust gapsAtEachLevel
+// and countsAtEachLevel (by hand).
+var quantizationStats = function () {
     var levelRanges = [];
     var numLevels = [];
-    var numLevelsSum = 0;
     var errors = [];
+    var sum = 0;
+    var numLevelsSum = 0;
 
     var k;
     for (k = 0; k < countsAtEachLevel.length; k++) {
         var count = countsAtEachLevel[k];
-        var quantize = quantizeAtEachLevel[k];
+        var gap = gapAtEachLevel[k];
 
-        var error = quantize / 2;
-        var errorFraction = error / (quantizations.length + error);
+        var error = gap / 2;
+        var errorFraction = error / (sum + error);
         var errorPercent = 100 * errorFraction;
         var errorRounded = Math.round(100 * errorPercent) / 100;
         errors[k] = errorRounded;
 
-        var j;
-        for (j = 0; j < count; j++) {
-            numLevelsSum++;
-            quantizations.push(1);
+        sum += count * gap;
+        numLevelsSum += count;
 
-            var i;
-            for (i = 1; i < quantize; i++) {
-                quantizations.push(0);
-            }
-        }
-
-        levelRanges[k] = quantizations.length;
+        levelRanges[k] = sum;
         numLevels[k] = numLevelsSum;
     }
 
-    console.log(quantizeAtEachLevel.map(Results.pad).join(''));
+    console.log(gapAtEachLevel.map(Results.pad).join(''));
     console.log(countsAtEachLevel.map(Results.pad).join(''));
     console.log(levelRanges.map(Results.pad).join(''));
     console.log(numLevels.map(Results.pad).join(''));
     console.log(errors.map(Results.pad).join(''));
-
-    return quantizations;
 };
 
 Quantizer.adjust = function (quantizations, position, lastPosition, lastAdjusted) {
